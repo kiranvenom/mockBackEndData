2f908b21330636c005dedb227e31523a17c0a9a9
kiranvenom <62389852+kiranvenom@users.noreply.github.com>
2024-02-25 12:14:48 +0530

    cors check

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const data = require('./data');
 const port = 5000;
 
 const app = express();
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
-	res.send('hi');
+    res.send('hi');
 });
 
 app.get('/userData', (req, res) => {
-	res.send(data);
+    res.json(data);
 });
 
 app.listen(port, () => {
-	console.log('runnning on port ' + port);
+    console.log('running on port ' + port);
 });
